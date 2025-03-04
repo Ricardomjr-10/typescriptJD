78dd559c082bc0f91b47550f55a208e5071764b0
Ricardo-10 <ricardomjr2@gmail.com>
2025-03-04 20:49:50 -0300

    array e tuplas

--- main.ts
+++ main.ts
@@ -1,1 +1,25 @@
-console.log('oi')
+//3 tipos basicos-string,number e boolean
+
+let nome: string = 'RIcardo'
+
+nome = 'daniel'
+
+console.log(nome)
+
+//arrays e tuplas
+
+let lista: string[]
+
+lista = ['daniel', 'Ricardo']
+
+//outro jeito
+
+let list: Array<number>
+
+list = [1, 2]
+
+//tupla
+
+let aluno: [string, number, boolean]
+
+aluno = ['daniel', 24, true]
