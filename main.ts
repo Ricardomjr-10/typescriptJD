7fe308ce3e9923fa76b1d334aec4749e6268ef1b
Ricardo-10 <ricardomjr2@gmail.com>
2025-03-04 21:14:46 -0300

    tipagem interface

--- main.ts
+++ main.ts
@@ -43,3 +43,27 @@ function soma(a: number,b: number): number {
 }
 
 const soma2 = (a: number, b: number): number => a + b // tipa depois do parenteses
+
+// union e type alias
+
+let id: number | string // union
+
+type Id = number | string // alias
+
+let id1: Id
+let id2: Id
+let id3: Id
+let id4: Id
+
+//type Usuario = { nome: string; idade: number }
+
+let user: Usuario
+
+// inerface
+
+interface Usuario { // a mesma coisa do type acima
+    nome: string
+    idade: number
+} // interface pra objetos e type para tipos basicos
+
+let user1: Usuario
