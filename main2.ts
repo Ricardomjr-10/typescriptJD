5eaf433c8e72c5db0d8a1412108edbeeccf2bc4c
Ricardo-10 <ricardomjr2@gmail.com>
2025-03-04 21:53:39 -0300

    generics com Arrays

--- main2.ts
+++ main2.ts
@@ -1,17 +1,30 @@
 //Generics
-type Pessoas = {
+type Pessoas<T = Aluno> = {
     nome: string
     idade: number
+    profissao: T
 }
 
-type Alunos = Pessoas & {
-    curso: string
+type Alunos ={
+    matricula: string
 }
 
-interface Engenheiro extends Pessoas {
+interface Engenheiro {
     crea: string
 }
 
-interface Medico extends Pessoas {
+interface Medico {
     crm: string
 }
+
+let medico: Pessoas<Medico>
+let alunos: Pessoas
+let engenheiro: Pessoas<Engenheiro>
+
+//medico.profissao.crm
+
+//com arrays
+
+type Listas<T = string> = T[]
+
+let listas: Listas
